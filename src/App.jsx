e6c0b0d3c60e9469bca16b1bb71ad4d1b957591d
OAlawaji <s202025780@kfupm.edu.sa>
2024-04-08 06:55:34 -0700

    First Push

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+import Note from "./note";
+import notes from "./notes.js";
+export default function App() {
+  return (
+    <div
+      className="
+    min-h-screen w-screen 
+    bg-secondary
+    text-background
+    "
+    >
+      <Header />
+      <main
+        className="
+        h-full overflow-auto
+        grid gap-20px
+        grid-cols-1
+        sm:grid-cols-2
+        md:grid-cols-3
+        lg:grid-cols-4
+        xl:grid-cols-5
+        2xl:grid-cols-6
+        justify-items-center items-center
+      "
+      >
+        {notes.map((note) => (
+          <Note key={note.key} title={note.title} content={note.content} />
+        ))}
+      </main>
+      <Footer />
+    </div>
+  );
+}
---
+++ src/header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header
+      className="
+      text-left 
+      bg-item
+      p-6
+      text-5xl
+      height-4
+      text-primary
+      font-semibold
+      tracking-wider
+      shadow-lg
+"
+    >
+      <h1>Notebook</h1>
+    </header>
+  );
+}
+
+export default Header;
